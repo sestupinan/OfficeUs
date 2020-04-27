90c9ae20bd6b4ca90e7478e3f45736213c5da5a4
SantiagoE <s.estupinan@uniandes.edu.co>
2020-04-27 18:04:11 -0500

    hola ojala no me lo tire

--- OfficeUs/OfficeUs/settings.py
+++ OfficeUs/OfficeUs/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'OfficeUs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'officeus',
-        'USER':'officeus',
-        'PASSWORD': 'officeus',
-        'HOST':'localhost',
-        'PORT':'',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
